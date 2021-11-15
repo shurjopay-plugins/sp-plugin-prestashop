27f543dc8e8be083269f1629f962970c4348df3f
naziaafsanmowmita@gmail.com <18-37787-2@student.aiub.edu>
2021-11-15 19:20:13 +0600

    Validate order

--- SHURJOPAY/controllers/front/validation.php
+++ SHURJOPAY/controllers/front/validation.php
@@ -30,51 +30,101 @@
 
 class ShurjopayValidationModuleFrontController extends ModuleFrontController
 {
-	/**
-	 * @see FrontController::postProcess()
-	**/
-	public function postProcess()
-	{
+    /**
+     * @see FrontController::postProcess()
+     **/
+    public function postProcess()
+    {
+
         $actual_link = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         $query_str = parse_url($actual_link, PHP_URL_QUERY);
         parse_str($query_str, $query_params);
-        $tran_id =$query_params['order_id'];
-	   // echo $tran_id = $_get['tran_id'];
+        $mainString = $query_params['controller'];
+        $cutPos = 20;
 
-	    $cart = $this->context->cart;
-	    $shurjopay = Module::getInstanceByName('SHURJOPAY');
+        $subString = substr($mainString, 0, $cutPos);
+        $restofString = substr($mainString, $cutPos, strlen($mainString));
+        $tran_id =$restofString;
 
-		if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)
-			Tools::redirect('index.php?controller=order&step=1');
-
-
-    
-		$customer = new Customer($cart->id_customer);
-		$currency = $this->context->currency;
-		$total = (float)$cart->getOrderTotal(true, Cart::BOTH);
-
-		$sp_mode = Configuration::get('MODE');
-		if (!Validate::isLoadedObject($customer))
-		{
-			Tools::redirect('index.php?controller=order&step=1');
-		}
-		
+        /*echo $restofString;exit;
+                echo ("<pre>");
+                print_r($query_params);
+                exit;*/
+        // echo $tran_id = $_get['tran_id'];
 
 
-        $objOrder = new Order($tran_id);
+
+        $cart = $this->context->cart;
+        $shurjopay = Module::getInstanceByName('SHURJOPAY');
+
+        if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)
+            Tools::redirect('index.php?controller=order&step=1');
+
+        // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
+        /*		$authorized = false;
+
+                foreach (Module::getPaymentModules() as $module)
+                {
+
+                    if ($module['name'] == 'SHURJOPAY')
+                    {
+                        $authorized = true;
+                        break;
+                    }
+
+                }
+                if (!$authorized)
+                {
+                    die($this->module->getTranslator()->trans('This payment method is not available.', array(), 'Modules.SHURJOPAY.Shop'));
+                }*/
+
+        $customer = new Customer($cart->id_customer);
+        $currency = $this->context->currency;
+        $total = (float)$cart->getOrderTotal(true, Cart::BOTH);
+        $order_id = Order::getOrderByCartId((int)($tran_id));
+        /*$val_id = $_POST['val_id'];*/
+
+// 		echo $tran_id."----".$order_id."----".$val_id;exit;
+
+        $sp_mode = Configuration::get('MODE');
+        if (!Validate::isLoadedObject($customer))
+        {
+            Tools::redirect('index.php?controller=order&step=1');
+        }
+
+        if( $sp_mode == 1 )
+        {
+            $valid_url_own = ("");
+        }
+        else
+        {
+            $valid_url_own = ("");
+        }
+
+        // echo $valid_url_own."<br>";
+
+        $objOrder = new Order($order_id);
         $history = new OrderHistory();
         $history->id_order = (int)$objOrder->id;
         $order_status = $objOrder->current_state;
+        // echo $history->id_order;
+        // echo $tran_id;
+        // echo $objOrder->id;
+        // print_r($_POST);
+        // print_r($history);
+        // exit;
 
-        
+        /*$success_URL = Tools::getHttpHost( true ).__PS_BASE_URI__.'index.php?controller=order-confirmation&id_cart='.$tran_id.'&id_module='.$this->module->id.'&id_order='.$history->id_order.'&key='.$customer->secure_key;*/
+        /*        $success_URL = Tools::getHttpHost( true ).__PS_BASE_URI__.'index.php?controller=history';*/
+        $failed_URL = Tools::getHttpHost( true ).__PS_BASE_URI__.'index.php?controller=history';
         $success_URL = Tools::getHttpHost( true ).__PS_BASE_URI__.'index.php?controller=order-confirmation&id_cart='.$tran_id.'&id_module='.$this->module->id.'&id_order='.$history->id_order.'&key='.$customer->secure_key;
-        $failed_URL = Tools::getHttpHost( true ).__PS_BASE_URI__.'index.php?controller=order-detail&id_cart='.$tran_id.'&id_module='.$this->module->id.'&id_order='.$history->id_order.'&key='.$customer->secure_key;
+
         $data['store_id'] = Configuration::get('SHURJOPAY_STORE_ID');
         $data['store_passwd'] = Configuration::get('SHURJOPAY_STORE_PASSWORD');
         $data['store_prefix'] = Configuration::get('SHURJOPAY_PREFIX');
         $data['engine_url'] = Configuration::get('ENGINE_URL');
 
-
+///
 
         $curl = curl_init();
 
@@ -85,9 +135,9 @@ class ShurjopayValidationModuleFrontController extends ModuleFrontController
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 0,
             CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS =>'{                                            "username": "'.$data['store_id'].'",
+            CURLOPT_POSTFIELDS =>'{
+                                            "username": "'.$data['store_id'].'",
                                             "password": "'.$data['store_passwd'].'"
                                         }',
             CURLOPT_HTTPHEADER => array(
@@ -114,7 +164,6 @@ class ShurjopayValidationModuleFrontController extends ModuleFrontController
                 CURLOPT_MAXREDIRS => 10,
                 CURLOPT_TIMEOUT => 0,
                 CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'POST',
                 CURLOPT_POSTFIELDS =>$tran_id,
                 CURLOPT_HTTPHEADER => array(
@@ -130,25 +179,33 @@ class ShurjopayValidationModuleFrontController extends ModuleFrontController
         $response=json_decode($response);
 
 
+
+
         $sp_code= $response[0]->sp_code;
+
         if($sp_code==1000)
         {
-            /* print_r($customer);
-             exit();*/
+
 
             $this->module->validateOrder($cart->id, Configuration::get('PS_OS_PAYMENT'), $total, $shurjopay->displayName, NULL, array(), (int)$currency->id, false, $customer->secure_key);
-            //exit();
-            $history->changeIdOrderState(2, (int)($objOrder->id)); //order status= Payment accepted
 
+            /* print_r($response);
+             exit;*/
+            $history->changeIdOrderState(2, (int)($objOrder->id));
             Tools::redirect($success_URL);
 
 
         }else{
-            $history->changeIdOrderState(6, (int)($objOrder->id));
+
+            $this->module->validateOrder($cart->id, Configuration::get('PS_OS_CANCELED'), $total, $shurjopay->displayName, NULL, array(), (int)$currency->id, false, $customer->secure_key);
+            $history->changeIdOrderState(8, (int)($objOrder->id));
+
+
 
             Tools::redirect($failed_URL);
 
         }
-        
-	}
+
+
+    }
 }
